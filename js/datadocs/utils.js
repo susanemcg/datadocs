39eb651f9c9cd7f61bcee623f1d28185f9d252ab
Susan E. McGregor <susan.e.mcgregor@gmail.com>
2014-01-10 13:37:47 -0500

    Added an addCommas function from Stack to format large numbers more readably.

--- js/datadocs/utils.js
+++ js/datadocs/utils.js
@@ -23,6 +23,25 @@ function monthNum(dateString){
 	
 }
 
+
+//lifted from Stack Overflow because why reinvent the wheel:
+//http://stackoverflow.com/questions/18286389/js-add-commas-to-a-string
+
+function addCommas(nStr)
+{
+    nStr += '';
+    x = nStr.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    
+    return(x1 + x2)
+}
+
+
 function monthLetter(monthNum){
 	var monthsArray = ["J", "F", "M", "A", "M", "J", "J", "A", "S", "O", "N", "D"];
 	return(monthsArray[monthNum])
